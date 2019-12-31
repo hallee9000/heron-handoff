203064c76745d807a989e007a6851f4d82e02cd0
leadream <leadream4@gmail.com>
2019-12-31 19:20:22 +0800

    添加下载进度条

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,26 @@
-import React, { createRef } from 'react'
+import React, { Fragment, createRef } from 'react'
 import JSZip from 'jszip'
 import { saveAs } from 'file-saver'
 import { Download, Settings } from 'react-feather'
-import { asyncForEach, toDataURL } from 'utils/helper'
+import { asyncForEach } from 'utils/helper'
 import { getSourceCode, getBufferData } from 'api'
 import './header.scss'
 
 class Header extends React.Component {
   img = createRef()
+  state = {
+    loaderWidth: 0,
+    loaderMessage: ''
+  }
   handleDownload = async () => {
     const { data, documentName } = this.props
     const zip = new JSZip()
-    console.log('开始')
+    this.setLoader(1, '处理 index.html')
     // generate html
     const indexSource = await getSourceCode(window.location.href)
     zip.file('index.html', indexSource.replace('var FILE_DATA=""', `var FILE_DATA = ${JSON.stringify(data)}`))
 
-    console.log('html done')
+    this.setLoader(3, '处理 js 文件')
     // generate js
     const js = zip.folder("static/js")
     const scripts = document.getElementsByTagName('script')
@@ -26,7 +30,7 @@ class Header extends React.Component {
       js.file(pieces[pieces.length - 1], jsSource)
     })
 
-    console.log('js done')
+    this.setLoader(15, `处理 css 文件`)
     // generate ico and css
     const css = zip.folder("static/css")
     const styles = document.getElementsByTagName('link')
@@ -41,35 +45,41 @@ class Header extends React.Component {
       }
     })
 
-    console.log('css ico done')
+    this.setLoader(26, `处理 logo.svg`)
     // generate logo.svg
-    const logoData = await toDataURL(this.img.current.src)
+    const logoData = await getBufferData(this.img.current.src)
     zip.file('logo.svg', logoData, {base64: true})
 
-    console.log('logo done')
+    this.setLoader(28, `处理图片`)
     // generate frame and component images
-    const dataFolder = zip.folder("data")
     const { images } = this.props
-    await asyncForEach(Object.keys(images), async id => {
-      const imgData = await toDataURL(images[id])
-      console.log(`${id} done`)
-      dataFolder.file(id.replace(':', '-') + 'png', imgData, {base64: true})
+    const dataFolder = zip.folder("data")
+    await asyncForEach(Object.keys(images), async (id, index) => {
+      const imgData = await getBufferData(`https://cors-anywhere.herokuapp.com/${images[id]}`)
+      this.setLoader(28+index*3, `${id}.png 完成`)
+      dataFolder.file(id.replace(':', '-') + '.png', imgData, {base64: true})
     })
 
-    console.log('images done')
+    this.setLoader(98, '生成压缩包')
     // generate zip
     zip.generateAsync({type: 'blob'})
       .then(function(content) {
         saveAs(content, `${documentName}.zip`)
       })
-    console.log('done!')
+    this.setLoader(100, `完成`)
+  }
+  setLoader = (percentage, message) => {
+    this.setState({
+      percentage,
+      message
+    })
   }
   hasNames = () => {
     const { pageName, frameName } = this.props
     return !!(pageName && frameName)
   }
   render () {
-    const { isLocal, documentName, pageName, frameName } = this.props
+    const { isLocal, documentName, pageName, frameName, loaderWidth, loaderMessage } = this.props
     return (
       <header className="app-header">
         <img className="header-logo" src={`${process.env.PUBLIC_URL}/logo.svg`} alt="logo" ref={this.img}/>
@@ -78,7 +88,11 @@ class Header extends React.Component {
         {
           this.hasNames() ?
           <span className="header-pagename">
-            {pageName}<span> / </span>{frameName}
+            {
+              (loaderWidth===0 || loaderWidth===100) ?
+              <Fragment>{pageName}<span> / </span>{frameName}</Fragment> :
+              loaderMessage
+            }
           </span> :
           <span className="header-pagename">Figma Handoff</span>
         }
@@ -93,6 +107,7 @@ class Header extends React.Component {
             </span>
           </div>
         }
+        <span className="header-loader" style={{width: `${loaderWidth}%`}}/>
       </header>
     )
   }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -74,26 +74,3 @@ export const getImage = (id, useLocalImages, images) =>
 
 export const getUrlImage = (id, useLocalImages, images) =>
   `url(${getImage(id, useLocalImages, images)})`
-
-export const toDataURL = (src, outputFormat) =>
-  new Promise((resolve, reject) => {
-    const img = new Image()
-    img.crossOrigin = 'Anonymous'
-    img.onload = function () {
-      const canvas = document.createElement('CANVAS')
-      const ctx = canvas.getContext('2d')
-      let dataURL
-      canvas.height = this.naturalHeight
-      canvas.width = this.naturalWidth
-      ctx.drawImage(this, 0, 0)
-      dataURL = canvas.toDataURL(outputFormat)
-      console.log(dataURL)
-      resolve(dataURL)
-    }
-    img.onerror = reject
-    img.src = src
-    if (img.complete || img.complete === undefined) {
-      img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="
-      img.src = src
-    }
-  })
